b2d4783599768c81fa7e8ae0d485f98086245f15
philips-of-ng@philips <philipsedun@gmail.com>
2024-11-12 11:36:45 +0100

    updated domain

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/My-Portfolio',
+  base: '/portfolio',
   server:{
     port: 5173
   },
